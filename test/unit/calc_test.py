f80af51a1aec8f6240861e9cb148bbf725406a11
David <dramono@gmail.com>
2025-01-27 14:41:32 +0100

    añado tests al fichero calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -39,6 +39,10 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
+        self.assertRaises(TypeError, self.calc.divide, None, 2)
+        self.assertRaises(TypeError, self.calc.divide, 2, None)
+        self.assertRaises(TypeError, self.calc.divide, object(), 2)
+        self.assertRaises(TypeError, self.calc.divide, 2, object())
 
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
