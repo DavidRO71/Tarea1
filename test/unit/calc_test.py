c118f6fd0ddc217636b6f5a5242d65c46d5ada90
David <dramono@gmail.com>
2025-01-30 17:56:15 +0100

    Añado otro test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -94,6 +94,9 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.substract, 2, None)
         self.assertRaises(TypeError, self.calc.substract, object(), 2)
         self.assertRaises(TypeError, self.calc.substract, 2, object())
+
+    def test_divide_method_error(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
